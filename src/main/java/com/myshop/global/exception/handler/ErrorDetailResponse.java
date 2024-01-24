c5e6002ca7b2bb16a825d885601b72ad08bebeeb
jaehyuuk <doogun97@gmail.com>
2024-01-24 21:06:22 +0900

    feat: GlobalExceptionHandler 및 ErrorResponse 추가

---
+++ src/main/java/com/myshop/global/exception/handler/ErrorDetailResponse.java
@@ -0,0 +1,14 @@
+package com.myshop.global.exception.handler;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class ErrorDetailResponse {
+    private String msg;
+
+    @Builder
+    public ErrorDetailResponse(String msg) {
+        this.msg = msg;
+    }
+}
