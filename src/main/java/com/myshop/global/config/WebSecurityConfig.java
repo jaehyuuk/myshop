3ca0f31ba763848549e6ccac3806e80da6d8b912
jaehyuuk <doogun97@gmail.com>
2024-01-24 21:05:35 +0900

    feat: spring security 및 jwt 설계

---
+++ src/main/java/com/myshop/global/config/WebSecurityConfig.java
@@ -0,0 +1,49 @@
+package com.myshop.global.config;
+
+import com.myshop.global.filter.JwtTokenFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsUtils;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+@Order(300)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtTokenFilter jwtTokenFilter;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable().cors()
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/api/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/**").permitAll()
+                .antMatchers(
+                        "/",
+                        "/ping",
+                        "/profile",
+                        "/webTest",
+                        "/active",
+                        "/favicon.ico",
+                        "/common/**",
+                        "/open-api/**",
+                        "/actuator/**",
+                        "/swagger-ui/**",
+                        "/v3/api-docs/**",
+                        "/swagger-ui.html")
+                .permitAll()
+                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .antMatcher("/**")
+                .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+}
---
+++ src/main/java/com/myshop/global/context/TokenContext.java
@@ -0,0 +1,10 @@
+package com.myshop.global.context;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TokenContext {
+    private Long userId;
+}
