00411b54950f40243883dbccf52073e1114dae29
jaehyuuk <doogun97@gmail.com>
2024-01-24 19:43:31 +0900

    feat: User Entity 추가

---
+++ src/main/java/com/myshop/domain/User.java
@@ -0,0 +1,39 @@
+package com.myshop.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@Getter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "profile_img")
+    private String profileImg;
+    @Column(name = "introduce")
+    private String introduce;
+
+    @Builder
+    public User(Long id, String name, String email, String password, String profileImg, String introduce) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.profileImg = profileImg;
+        this.introduce = introduce;
+    }
+
+}
