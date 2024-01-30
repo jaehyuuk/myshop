b69f13c34f25bfd111ec5fe3f2cda8f8f5194e5d
jaehyuuk <doogun97@gmail.com>
2024-01-30 13:35:37 +0900

    refactor: enum을 활용하여 알림 Dto 리팩토링

--- src/main/java/com/myshop/domain/NotiType.java
+++ src/main/java/com/myshop/domain/NotiType.java
@@ -4,11 +4,34 @@ import lombok.Getter;
 
 @Getter
 public enum NotiType {
-    LIKE("좋아요"), COMMENT("댓글"), FOLLOW("팔로우");
+    LIKE("좋아") {
+        @Override
+        public String formatMessage(String fromUserName, String toUserName, boolean isMyNotification) {
+            String targetUserName = isMyNotification ? "당신의" : toUserName + "님의";
+            return String.format("%s님이 %s 포스트를 %s합니다", fromUserName, targetUserName, getKey());
+        }
+    },
+    COMMENT("댓글") {
+        @Override
+        public String formatMessage(String fromUserName, String toUserName, boolean isMyNotification) {
+            String targetUserName = isMyNotification ? "당신의" : toUserName + "님의";
+            return String.format("%s님이 %s 포스트에 %s를 남겼습니다", fromUserName, targetUserName, getKey());
+        }
+    },
+    FOLLOW("팔로우") {
+        @Override
+        public String formatMessage(String fromUserName, String toUserName, boolean isMyNotification) {
+            String targetUserName = isMyNotification ? "당신을" : toUserName + "님을";
+            return String.format("%s님이 %s %s합니다", fromUserName, targetUserName, getKey());
+        }
+    };
 
     NotiType(String key) {
         this.key = key;
     }
 
     private String key;
+
+    public abstract String formatMessage(String fromUserName, String toUserName, boolean isMyNotification);
+
 }
--- src/main/java/com/myshop/dto/NotificationDto.java
+++ src/main/java/com/myshop/dto/NotificationDto.java
@@ -14,24 +14,18 @@ public class NotificationDto {
     private String message;
     private LocalDateTime createdAt;
 
-    public static NotificationDto getNotification(Notification notification) {
-        String message = "";
-        message += notification.getFromUser().getName();
-        message += "님이 ";
-        message += notification.getToUser().getName();
-        if (notification.getType().equals(FOLLOW)) message += "님을 팔로우 합니다.";
-        if (notification.getType().equals(COMMENT)) message += "님의 포스트에 댓글을 남겼습니다.";
-        if (notification.getType().equals(LIKE)) message += "님의 포스트를 좋아합니다.";
-        return new NotificationDto(message, notification.getCreatedAt());
+    public static NotificationDto getFollowNotification(Notification notification) {
+        String fromUserName = notification.getFromUser().getName();
+        String toUserName = notification.getToUser().getName();
+        String formattedMessage = notification.getType().formatMessage(fromUserName, toUserName, false);
+
+        return new NotificationDto(formattedMessage, notification.getCreatedAt());
     }
 
     public static NotificationDto getMyNotification(Notification notification) {
-        String message = "";
-        message += notification.getFromUser().getName();
-        message += "님이 ";
-        if (notification.getType().equals(FOLLOW)) message += "당신을 팔로우 합니다.";
-        if (notification.getType().equals(COMMENT)) message += notification.getTypeId() + " 포스트에 댓글을 남겼습니다.";
-        if (notification.getType().equals(LIKE)) message += notification.getTypeId() + " 포스트를 좋아합니다.";
-        return new NotificationDto(message, notification.getCreatedAt());
+        String fromUserName = notification.getFromUser().getName();
+        String formattedMessage = notification.getType().formatMessage(fromUserName, "", true);
+
+        return new NotificationDto(formattedMessage, notification.getCreatedAt());
     }
 }
